393a80a7a20fcfc9e43f69bee7f7a47769d42367
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2024-01-10 15:16:19 -0600

    set up some of the file structure / commiting to change branches

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+// file is used to export all components from one place
+export { default  as HeroSection } from './hero_section/HeroSection'
+export { default as Navbar } from './navbar/NavBar'
+export { default as WorkHistory } from './work_history/WorkHistory'
+export { default as SkillsSection } from './skills_section/SkillsSection'
---
+++ src/containers/index.ts
@@ -0,0 +1,3 @@
+export { default as Header } from './header/Header'
+export { default as sidebar } from '../containers/sidebar/SideBar'
+export { default as Footer } from '../containers/footer/Footer'
