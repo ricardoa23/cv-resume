28e6289658ae718e3b01e8c47ca28c2abd5b85ba
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2024-01-12 16:12:06 -0600

    commiting to switch branches

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -16,27 +16,6 @@ const SideBar = () => {
     <div className="text-5xl text-center text-accent2 pb-2" >Ricardo Ayala</div>
       <SkillsSection title="Tech Skills"/>
       <SkillsSection title="Soft Skills"/>
-      <div className="text-4xl text-accent3 text-center">Education</div>
-      <div className="flex justify-around">
-        <div className="flex flex-col">
-        <div className="text-2xl p-2 text-center">CodeUp</div>
-        <a
-                href="#"
-                className=" border border-solid border-white rounded-md min-w-[120px] bg-accent2 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-accent1 text-center"
-              >
-                Education
-              </a>
-        </div>
-        <div className="flex flex-col">
-        <div className="text-2xl p-2 text-center">Bachelor's</div>
-        <a
-                href="#"
-                className=" border border-solid border-white rounded-md min-w-[120px] bg-accent2 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-accent1 text-center"
-              >
-                Education
-              </a>
-        </div>
-      </div>
       </div>
   );
 }
